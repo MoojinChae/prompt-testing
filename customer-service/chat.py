a2db3dfde7dced878d1911dcea5b9aa7c71724d4
Moojin Chae <moojin@Moojins-MacBook-Pro.local>
2024-01-04 15:10:06 -0800

    Add basic customer service prompts

---
+++ customer-service/chat.py
@@ -0,0 +1,52 @@
+from openai import OpenAI
+
+client = OpenAI(
+    api_key="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",
+)
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model=model,
+        temperature=1,
+    )
+    return chat_completion
+
+size = 3
+field = "hotel booking system"
+
+output_format = f"""
+Provide them in JSON format with the following keys: 
+name: <customer name>
+email: <customer email>
+agent: <agent name>
+wait time: <wait time to start conversation with agent>
+total tile: <total coverstaion time>
+summary: <summary of coversation>
+chat: <list of coversation between agent and customer>
+status: <one of 'created', 'resolved', 'investigating'>
+ticket: <optional field. create a jira ticket number only if status is 'investigating'>
+created_at: <chat creation time>
+"""
+
+prompt = f"""
+I am running {field} business and I would like to get a fake customer service chat conversation \
+to do quality assurance. Here are few requirements that I have for you.
+1. Cover many cases as much as possible.
+2. 10% of customers are really upset due to the issue they have.
+3. 30% of issues is not resolved the issue during coversasion which will create a issue ticket to follow up.
+{output_format}
+"""
+print("====prompt====")
+print(prompt)
+for i in range(size):
+    response = get_completion(prompt)
+    print(f"====response-{i}====")
+    print(response.choices[0].message.content)
+    print("====token====")
+    print(response.usage)
---
+++ customer-service/contact-us.py
@@ -0,0 +1,60 @@
+from openai import OpenAI
+
+client = OpenAI(
+    api_key="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",
+)
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model=model,
+        temperature=1,
+    )
+    return chat_completion
+
+size = 3
+field = "hotel booking"
+
+output_format = f"""
+Provide them in JSON format with the following keys: 
+name: <customer name>
+email: <customer email>
+title: <form title>
+description: <issue descriptions very preciously. up to 5 sentences>
+attachments: <optional. It could be screenshots image file names regarding {field}>
+status: <one of 'created', 'in process', 'resolved'>
+created_at: <form creation time>
+"""
+
+# prompt = f"""
+# Your task is to create {size} "Contact Us" forms that users submit for their issues regarding {field}.
+# {output_format}
+# """
+
+# prompt = f"""
+# I am running {field} business and I would like to get {size} "Contact Us" forms that \
+# users submit regarding their issues.
+# {output_format}
+# """
+
+prompt = f"""
+I am running {field} business and I would like to get a fake "Contact Us" form \
+that users submit regarding their issues to do quality assurance. Here are few requirements that I have for you.
+1. Cover many cases as much as possible.
+2. Some customers could possible be really upset due to the issue they have.
+{output_format}
+"""
+
+print("====prompt====")
+print(prompt)
+for i in range(size):
+    response = get_completion(prompt)
+    print(f"====response-{i}====")
+    print(response.choices[0].message.content)
+    print("====token====")
+    print(response.usage)
